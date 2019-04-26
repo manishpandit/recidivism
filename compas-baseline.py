8c09a3a93d503ab9e0f9e3306f3b06c02dbb7798
Jonathan Hollenbeck <jonoepd@gmail.com>
2019-04-25 21:51:34 -0700

    Remove correlated features from all-xy.csv, implement preprocessing fairness algorithm from aif360 package

--- compas-baseline.py
+++ compas-baseline.py
@@ -2,6 +2,11 @@
 import pandas as pd
 import numpy as np
 
+def convert_columns(df, conversion_dict): # modify df in place
+    for col_key in conversion_dict.keys():
+        key_dict = conversion_dict[col_key]
+        df[col_key] = df[col_key].apply(lambda x: key_dict[x])
+
 violent_file = 'compas-scores-two-years-violent.csv'
 all_file = 'compas-scores-two-years.csv'
 
@@ -12,23 +17,18 @@ y = np.ndarray.astype(all_df.values[:,-1],int)
 column_list = [
     'id', 'sex','age','age_cat',
     'juv_fel_count','decile_score','juv_misd_count','juv_other_count',
-    'priors_count','is_recid','is_violent_recid',
+    'priors_count',
     'decile_score.1', 'score_text', 'v_decile_score', 'v_score_text',
     'priors_count.1'
 ]
 # todo: add categorical variables (race). consider specific analysis for crime category / crime text
-# also should think about meaning of columns with NaN 
+# also should think about meaning of columns with NaN
 conversion_dict = {
     'sex': { 'Female': 1, 'Male': 0 },
     'age_cat': { 'Less than 25': -1, '25 - 45': 0, 'Greater than 45': 1},
     'score_text': { 'High': 1, 'Low': -1, 'Medium': 0 },
     'v_score_text': { 'High': 1, 'Low': -1, 'Medium': 0 }
 }
-
-def convert_columns(df, conversion_dict): # modify df in place
-    for col_key in conversion_dict.keys():
-        key_dict = conversion_dict[col_key]
-        df[col_key] = df[col_key].apply(lambda x: key_dict[x])
 
 Xy_df = all_df[column_list]
 convert_columns(Xy_df, conversion_dict)
--- compas-logreg.py
+++ compas-logreg.py
@@ -1,4 +1,5 @@
 # simple example of logistic regression on cleaned data
+# balance using ai package and compare results
 import pandas as pd
 import numpy as np
 import sklearn
@@ -12,23 +13,131 @@ def min_max_scaling(X):
     scalar_fit = scalar.fit(X)
     dmin = scalar.data_min_
     dmax = scalar.data_max_
-    return scalar.transform(X)
+    return scalar
 
 # load data
 data_file = 'all-xy.csv'
 Xy_df = pd.read_csv(data_file)
-y = np.ndarray.astype(Xy_df.values[:,-1],int)
-X_df = Xy_df.drop(Xy_df.columns[[0,-1]],axis=1)
-X = np.ndarray.astype(X_df.values,float)
 
-# train model
-Xnorm = min_max_scaling(X)
-X_train, X_test, y_train, y_test = train_test_split(Xnorm,y,test_size=0.2,random_state=42)
-clf = LR(penalty='l2', class_weight='balanced').fit(X_train, y_train)
-train_preds = clf.predict_proba(X_train)[:,1]
-test_preds = clf.predict_proba(X_test)[:,1]
+# prep train/dev datasets
+Xy_train, Xy_test = train_test_split(Xy_df,test_size=0.2,random_state=42)
+y_train = np.ndarray.astype(Xy_train.values[:,-1],int)
+X_train = Xy_train.drop(Xy_train.columns[[0,-1]],axis=1)
+y_test = np.ndarray.astype(Xy_test.values[:,-1],int)
+X_test = Xy_test.drop(Xy_test.columns[[0,-1]],axis=1)
+scalar = min_max_scaling(X_train)
+X_train_norm = scalar.transform(X_train)
+X_test_norm = scalar.transform(X_test)
+
+# train initial model
+clf = LR(penalty='l2', class_weight='balanced').fit(X_train_norm, y_train)
+train_preds = clf.predict_proba(X_train_norm)[:,1]
+test_preds = clf.predict_proba(X_test_norm)[:,1]
+test_preds_label = clf.predict(X_test_norm)
 class_preds = test_preds >= 0.5
 train_ll = log_loss(y_train, train_preds)
 test_ll = log_loss(y_test, test_preds)
 print('test log-loss: ' + str(test_ll))
-#confusion_matrix(y_test, class_preds)
+
+# working with ai360 -- first, run on initial model (no debiasing)
+from aif360.datasets import StandardDataset
+from aif360.datasets import BinaryLabelDataset
+from aif360.metrics import ClassificationMetric
+
+def fairness_metrics(classified_metric, log=False): # prints instead of return if log=True
+    TPR = classified_metric.true_positive_rate()
+    TNR = classified_metric.true_negative_rate()
+    bal_acc = np.round(0.5*(TPR+TNR),2)
+    dip = np.round((1/classified_metric.disparate_impact() - 1)*100,2)
+    aod = np.round(classified_metric.average_odds_difference(),2)
+    spd = np.round(classified_metric.statistical_parity_difference(),2)
+    eod = np.round(classified_metric.equal_opportunity_difference(),2)
+    if log:
+        print("Balanced Accuracy: " + str(bal_acc))
+        print("Disparate Impact (%): " + str(dip))
+        print("Average Odds Difference: " + str(aod))
+        print("Statistical Parity Difference: " + str(spd))
+        print("Equal Opportunity Difference: " + str(eod))
+    else:
+        return bal_acc,dip,aod,spd,eod
+
+
+privileged_groups = [{'sex': 1}]
+unprivileged_groups = [{'sex': 0}]
+
+comp_dataset = BinaryLabelDataset(
+    df=Xy_test, favorable_label=0, unfavorable_label = 1,
+    label_names = ['y'], protected_attribute_names = ['sex'],
+    privileged_protected_attributes = [[0]],
+    unprivileged_protected_attributes = [[1]]
+)
+comp_dataset_pred = comp_dataset.copy()
+comp_dataset_pred.labels = test_preds_label
+comp_dataset_pred.scores = test_preds
+
+classified_metric = ClassificationMetric(
+    comp_dataset, comp_dataset_pred, unprivileged_groups = unprivileged_groups,
+    privileged_groups = privileged_groups)
+
+# repeat with preprocessing
+#from aif360.algorithms.preprocessing.optim_preproc import OptimPreproc
+#from aif360.algorithms.preprocessing.optim_preproc_helpers.opt_tools import OptTools
+from aif360.algorithms.preprocessing.reweighing import Reweighing
+
+cd_train = BinaryLabelDataset(
+    df=Xy_train, favorable_label=0, unfavorable_label = 1,
+    label_names = ['y'], protected_attribute_names = ['sex'],
+    privileged_protected_attributes = [[0]],
+    unprivileged_protected_attributes = [[1]]
+)
+cd_test = BinaryLabelDataset(
+    df=Xy_test, favorable_label=0, unfavorable_label = 1,
+    label_names = ['y'], protected_attribute_names = ['sex'],
+    privileged_protected_attributes = [[0]],
+    unprivileged_protected_attributes = [[1]]
+)
+
+RW = Reweighing(unprivileged_groups=unprivileged_groups,
+               privileged_groups=privileged_groups)
+RW.fit(cd_train)
+Xyt_train = RW.transform(cd_train)
+Xyt_test = RW.transform(cd_test)
+
+# prep train/dev datasets
+yt_train = Xyt_train.labels.ravel()
+Xt_train = Xyt_train.features
+yt_test = Xyt_test.labels.ravel()
+Xt_test = Xyt_test.features
+scalar_fair = min_max_scaling(Xt_train)
+Xt_train_norm = scalar_fair.transform(Xt_train)
+Xt_test_norm = scalar_fair.transform(Xt_test)
+
+# train initial model
+clf = LR(penalty='l2', class_weight='balanced').fit(
+    Xt_train_norm, yt_train, sample_weight=Xyt_train.instance_weights)
+fair_train_preds = clf.predict_proba(Xt_train_norm)[:,1]
+fair_test_preds = clf.predict_proba(Xt_test_norm)[:,1]
+fair_test_preds_label = clf.predict(Xt_test_norm)
+fair_class_preds = fair_test_preds >= 0.5
+train_ll = log_loss(yt_train, fair_train_preds)
+test_ll = log_loss(yt_test, fair_test_preds)
+print('test log-loss: ' + str(test_ll))
+
+# calculate fairness metrics for rebalanced model
+fair_comp_dataset = BinaryLabelDataset(
+    df=Xy_test, favorable_label=0, unfavorable_label = 1,
+    label_names = ['y'], protected_attribute_names = ['sex'],
+    privileged_protected_attributes = [[0]],
+    unprivileged_protected_attributes = [[1]]
+)
+fair_comp_dataset_pred = fair_comp_dataset.copy()
+fair_comp_dataset_pred.labels = fair_test_preds_label
+fair_comp_dataset_pred.scores = fair_test_preds
+
+fair_classified_metric = ClassificationMetric(
+    fair_comp_dataset, fair_comp_dataset_pred, unprivileged_groups = unprivileged_groups,
+    privileged_groups = privileged_groups)
+
+# finally, compare!
+fairness_metrics(classified_metric, log=True)
+fairness_metrics(fair_classified_metric, log=True)
